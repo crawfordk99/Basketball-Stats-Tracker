978484e09e68e74d490ed9eab92b76de578ad84e
Keith Crawford <keithacrawfordjr@gmail.com>
2024-03-26 13:39:44 -0600

    Set up my new dotnet project for the final project

---
+++ FieldGoal.cs
@@ -0,0 +1,4 @@
+public abstract class FieldGoal {
+    private int _shotsMade;
+    private int _shotsAttempted;
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args){
+        Console.WriteLine("Welcome to the stats side of basketball. This program "+
+        "This program will help you to keep track of your basic counting " +
+        "and efficiency stats. This will help you to see where you could use improvement " +
+        "and set goals off of that. We'll also build your dream profile if you were to" +
+        "to make it to the NBA.")
+    }
+}
